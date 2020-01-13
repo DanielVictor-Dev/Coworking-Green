f05fc6389a580fe7b674232312ebef75b0ae82f9
Daniel Victor <victorcapela7@gmail.com>
2020-01-12 22:29:25 -0300

    added Site Coworking Green

---
+++ js/main.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+
+    var scrollLink = $('.scroll');
+
+    // Smooth scrolling
+    scrollLink.click(function (e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 2000);
+    });
+
+    // Active link switching
+    $(window).scroll(function () {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function () {
+
+            var sectionOffset = $(this.hash).offset().top - 20;
+
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+
+            }
+        })
+
+    })
+
+})
+
+
+
+
+/******** Number animation *********/
+let nCount = selector => {
+    $(selector).each(function () {
+        $(this)
+            .animate({
+                Counter: $(this).text()
+            }, {
+                // A string or number determining how long the animation will run.
+                duration: 3000,
+                // A string indicating which easing function to use for the transition.
+                easing: "swing",
+                /**
+                 * A function to be called for each animated property of each animated element. 
+                 * This function provides an opportunity to
+                 *  modify the Tween object to change the value of the property before it is set.
+                 */
+                step: function (value) {
+                    $(this).text(Math.ceil(value));
+                }
+            });
+    });
+};
+
+let a = 0;
+$(window).scroll(function () {
+    // The .offset() method allows us to retrieve the current position of an element  relative to the document
+    let oTop = $(".numbers").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() >= oTop) {
+        a++;
+        nCount(".rect > h1");
+    }
+});
+
+
+/******** Start button responsive *********/
+
+$("#wrapper").click(function () {
+    $(".icon").toggleClass("close");
+});
